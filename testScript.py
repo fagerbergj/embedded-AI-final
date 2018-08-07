ed5dd47adfb876a6cfe999309a3de9687b5cc2af
Jason Fagerberg <fagerbergj1@wit.edu>
2018-08-07 14:08:34 -0400

    space fixes

--- testScript.py
+++ testScript.py
@@ -23,47 +23,39 @@ time.sleep(2.0)
 
 # loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-    frame = imutils.resize(frame, width=400)
+        # grab the frame from the threaded video stream and resize it
+    	# to have a maximum width of 400 pixels
+    	frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        # prepare the image to be classified by our deep learning network
+    	image = cv2.resize(frame, (28, 28))
+    	image = image.astype("float") / 255.0
+    	image = img_to_array(image)
+    	image = np.expand_dims(image, axis=0)
+        # classify the input image and initialize the label and
+    	# probability of the prediction
+    	(robot, human) = model.predict(image)[0]
+    	label = "Robot"
+    	proba = robot
+        # check to see if santa was detected using our convolutional
+    	# neural network
+    	if human > robot:
+            # update the label and prediction probability
+            label = "Human"
+            proba = human
+        if human < 50 and robot < 50:
+            label = "Unable to determine"
+            proba = 0
+        # build the label and draw it on the frame
+    	label = "{}: {:.2f}%".format(label, proba * 100)
+    	frame = cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    	# show the output frame
+    	cv2.imshow("Frame", frame)
+    	key = cv2.waitKey(1) & 0xFF
+    	# if the `q` key was pressed, break from the loop
+    	if key == ord("q"):
+            break
 
-    # prepare the image to be classified by our deep learning network
-	image = cv2.resize(frame, (28, 28))
-	image = image.astype("float") / 255.0
-	image = img_to_array(image)
-	image = np.expand_dims(image, axis=0)
-
-    # classify the input image and initialize the label and
-	# probability of the prediction
-	(robot, human) = model.predict(image)[0]
-	label = "Robot"
-	proba = robot
-
-    # check to see if santa was detected using our convolutional
-	# neural network
-	if human > robot:
-		# update the label and prediction probability
-		label = "Human"
-		proba = human
-
-    if human < 50 and robot < 50:
-        label = "Unable to determine"
-        proba = 0
-
-    # build the label and draw it on the frame
-	label = "{}: {:.2f}%".format(label, proba * 100)
-	frame = cv2.putText(frame, label, (10, 25),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
-	# show the output frame
-	cv2.imshow("Frame", frame)
-	key = cv2.waitKey(1) & 0xFF
-
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
-        
 # do a bit of cleanup
 print("[INFO] cleaning up...")
 cv2.destroyAllWindows()
