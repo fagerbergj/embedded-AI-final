7f1f8012a4c978d6f43d7676278ff516368ae6f4
fagerbergj <fagerbergj1@wit.edu>
2018-08-08 10:46:45 -0400

    actual app done

--- testScript.py
+++ testScript.py
@@ -28,27 +28,35 @@ while True:
     frame = vs.read()
     frame = imutils.resize(frame, width=400)
     # prepare the image to be classified by our deep learning network
-    image = cv2.resize(frame, (28, 28))
+    image = cv2.resize(frame, (96, 96))
     image = image.astype("float") / 255.0
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
+    r = 0
+    g = 0
+    b = 0
     # classify the input image and initialize the label and
     # probability of the prediction
     (robot, human) = model.predict(image)[0]
     label = "Robot"
     proba = robot
-    # check to see if santa was detected using our convolutional
-    # neural network
-    if human > robot:
+
+    if robot > human:
+        r = 255
+        # check to see if santa was detected using our convolutional
+        # neural network
+    elif human > robot:
         # update the label and prediction probability
         label = "Human"
         proba = human
-    if human < 50 and robot < 50:
-        label = "Unable to determine"
-        proba = 0
+        g = 255
+    elif human < .7 and robot < .7:
+        label = "WHAT ARE YOU?!?!?!?"
+        b = 255
+
     # build the label and draw it on the frame
     label = "{}: {:.2f}%".format(label, proba * 100)
-    frame = cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+    frame = cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (b, g, r), 2)
     # show the output frame
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
