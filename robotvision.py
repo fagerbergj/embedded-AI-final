5e00963a24421f89dcda59468f3cd4286653cce6
fagerbergj <fagerbergj1@wit.edu>
2018-08-08 12:38:05 -0400

    all cv2 now no imutils

--- robotvision.py
+++ robotvision.py
@@ -1,7 +1,6 @@
 # import the necessary packages
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
-from imutils.video import VideoStream
 from threading import Thread
 import numpy as np
 import time
@@ -16,15 +15,15 @@ model = load_model(MODEL_PATH)
 
 # initialize the video stream and allow the camera sensor to warm up
 print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
+vs = cv2.VideoCapture(0)
 time.sleep(2.0)
 
 # loop over the frames from the video stream
 while True:
     # grab the frame from the threaded video stream and resize it
     # to have a maximum width of 400 pixels
-    frame = vs.read()
+    f = vs.read()
+    frame = f[1]
     # prepare the image to be classified by our deep learning network
     image = cv2.resize(frame, (96, 96))
     image = image.astype("float") / 255.0
@@ -55,10 +54,10 @@ while True:
     # build the label and draw it on the frame
     label = "{}: {:.2f}%".format(label, proba * 100)
     frame = cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (b, g, r), 2)
-    
-    frame = cv2.copyMakeBorder(frame, 10, 10, 10, 10, cv2.BORDER_CONSTANT, (b, g, r))
+    border = cv2.copyMakeBorder(frame, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=[b, g, r])
     # show the output frame
-    cv2.imshow("Frame", frame)
+    #cv2.imshow("Frame", frame)
+    cv2.imshow("border", border)
     key = cv2.waitKey(1) & 0xFF
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
@@ -66,5 +65,5 @@ while True:
 
 # do a bit of cleanup
 print("[INFO] cleaning up...")
+vs.release()
 cv2.destroyAllWindows()
-vs.stop()
