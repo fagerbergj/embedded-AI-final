e1b45538e91574fd3e516e1409d08eab056bf927
Jason Fagerberg <fagerbergj1@wit.edu>
2018-08-09 00:36:54 -0400

    ready to present

--- robotvision.py
+++ robotvision.py
@@ -7,9 +7,16 @@ import time
 import cv2
 import os
 import pickle
+import argparse
 
-MODEL_PATH = "Robots1.model"
-LABEL_PATH = "lb.pickle"
+# get model and label paths from user
+ap = argparse.ArgumentParser()
+ap.add_argument("-m", "--model", required=True, help = "path to input model")
+ap.add_argument("-l", "--labels", required=True, help="path to onput label binarizer")
+args = vars(ap.parse_args())
+
+MODEL_PATH = args["model"]
+LABEL_PATH = args["labels"]
 
 # load the model
 print("[INFO] loading model...")
@@ -21,27 +28,31 @@ print("[INFO] starting video stream...")
 vs = cv2.VideoCapture(0)
 time.sleep(2.0)
 
-width = int(vs.get(3))  # float
-height = int(vs.get(4)) # float
+width = int(vs.get(3))  # width of frame
+height = int(vs.get(4)) # height of frame
 
-widthMul = 3
+# muls effect size of crosshair, bigger muls -> bigger crosshair
+widthMul = 4
 heightMul = 7
+# percent of screen covered by crosshair
 widthSlice = int(width/widthMul)
 heightSlice = int(height/heightMul)
 
 # loop over the frames from the video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to have a maximum width of 400 pixels
+    # grab the frame from the threaded video stream
     f = vs.read()
     frame = f[1]
+
     # prepare the image to be classified by our deep learning network
+    # only take image in crosshair
     image = frame[heightSlice:(heightMul-1) * heightSlice, widthSlice : (widthMul-1) * widthSlice , :]
-    #print(image.shape)
+    # rezise image to 96x96
     image = cv2.resize(image, (96, 96))
     image = image.astype("float") / 255.0
     image = img_to_array(image)
     image = np.expand_dims(image, axis=0)
+    # color values for ui
     r = 0
     g = 0
     b = 0
@@ -62,15 +73,15 @@ while True:
     # build the label and draw it on the frame
     label = "{}: {:.2f}%".format(label, proba[idx] * 100)
     frame = cv2.putText(frame, label, (10, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (b, g, r), 2)
-    border = cv2.copyMakeBorder(frame, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=[b, g, r])
+    frame = cv2.copyMakeBorder(frame, 10, 10, 10, 10, cv2.BORDER_CONSTANT, value=[b, g, r])
 
-    lineThickness = 12
-    cv2.line(border, (widthSlice, int(height/2)), ((widthMul - 1)* widthSlice, int(height/2)), (b,g,r), lineThickness)
-    cv2.line(border, (int(width/2), heightSlice), (int(width/2), (heightMul - 1) * heightSlice), (b,g,r), lineThickness)
+    # draw crosshair
+    lineThickness = 8
+    cv2.line(frame, (widthSlice, int(height/2)), ((widthMul - 1)* widthSlice, int(height/2)), (b,g,r), lineThickness)
+    cv2.line(frame, (int(width/2), heightSlice), (int(width/2), (heightMul - 1) * heightSlice), (b,g,r), lineThickness)
 
     # show the output frame
-    #cv2.imshow("Frame", frame)
-    cv2.imshow("border", border)
+    cv2.imshow("frame", frame)
     key = cv2.waitKey(1) & 0xFF
     # if the `q` key was pressed, break from the loop
     if key == ord("q"):
